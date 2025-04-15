66a91584618439deee63e95889ecb85c60b5bf5f
Cyfer-ap <146387998+Cyfer-ap@users.noreply.github.com>
2025-04-15 10:27:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (44.3%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles>=numExchange) {ans++;numBottles-=numExchange-1;}
+        return ans;
+    }
+};
