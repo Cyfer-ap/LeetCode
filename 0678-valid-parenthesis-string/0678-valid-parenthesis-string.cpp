c036359b68d43bd9d31bf3a5b556a7bafcbf2e20
Abhinav Pathak <abhinavpathak789@gmail.com>
2025-07-05 03:04:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (46.83%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                low++;
+                high++;
+            }
+            else if (s[i] == ')'){
+                low--;
+                high--;
+            }
+            else{
+                low--;
+                high++;
+            }
+
+            if(low < 0) low = 0;
+
+            if (high < 0) return false;
+
+        }
+        return (low == 0);
+    }
+};
