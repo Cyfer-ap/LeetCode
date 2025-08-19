0481ee056366e400d7fdaf8058921bc6cb7919b4
Abhinav Pathak <abhinavpathak789@gmail.com>
2025-08-19 17:18:46 +0530

    Time: 23 ms (98.39%), Space: 13.7 MB (83.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if (total & 1) return false;
-        int T = total / 2;
-        int n = (int)nums.size();
-
-        vector<vector<int8_t>> dp(n, vector<int8_t>(T + 1, -1));
-
-        function<int8_t(int,int)> dfs = [&](int i, int need) -> int8_t {
-            if (need == 0) return 1;
-            if (i < 0)     return 0;
-            int8_t &res = dp[i][need];
-            if (res != -1) return res;
-
-            if (dfs(i - 1, need)) return res = 1;
-            if (nums[i] <= need && dfs(i - 1, need - nums[i])) return res = 1;
-
-            return res = 0;
-        };
-
-        return dfs(n - 1, T);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) return false;
+        int T = sum / 2;
+        vector<char> dp(T+1, 0);
+        dp[0] = 1;
+        for(int x : nums){
+            for(int s = T; s >= x; --s){
+                dp[s] = dp[s] || dp[s-x];
+            }
+            if(dp[T]) return true;
+        }
+        return dp[T];
     }
 };
