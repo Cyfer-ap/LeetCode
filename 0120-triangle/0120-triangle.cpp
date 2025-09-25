bfb712460146f0323c918abfcae3f919377a94cb
Abhinav Pathak <abhinavpathak789@gmail.com>
2025-09-25 13:59:51 +0530

    Time: 2 ms (16.49%), Space: 12.8 MB (72.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -7,6 +7,7 @@ public:
         vector<int> dp = triangle.back();
 
         for(int i=rows-2; i >= 0; --i){
+            
             for(int j=0; j<=i; ++j){
                 dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
             }
